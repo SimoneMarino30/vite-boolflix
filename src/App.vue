278ea62a59562e8cabad95a8f101a0141f871015
Simone Marino <simo.marino@outlook.it>
2023-03-03 17:02:05 +0100

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+  data() {
+    return {
+      title: "Hello world",
+    };
+  },
+
+  components: { AppHeader },
+};
+</script>
+
+<template>
+  <h1 class="text-danger">{{ title }}</h1>
+  <AppHeader />
+</template>
+
+<style lang="scss"></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script></script>
+
+<template>
+  <div class="input-group mb-3">
+    <input
+      type="text"
+      class="form-control"
+      placeholder="Recipient's username"
+      aria-label="Recipient's username"
+      aria-describedby="button-addon2"
+    />
+    <button class="btn btn-outline-secondary" type="button" id="button-addon2">
+      Search movies
+    </button>
+  </div>
+</template>
+
+<style lang="scss"></style>
